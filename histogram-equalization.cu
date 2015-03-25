d3547e0a95d6224259b958f12924c87de4b2e966
kptnkrnch <jckptnkrnch@gmail.com>
2015-03-25 15:33:04 -0700

    Files I've been working on porting
    I'm using CU files because I'm working on Windows with Visual Studio,
    not sure if nvcc will compile them on linux. They're just renamed .cpp
    and .h files

---
+++ histogram-equalization.cu
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "hist-equ.cuh"
+
+
+void histogram(int * hist_out, unsigned char * img_in, int img_size, int nbr_bin){
+    int i;
+    for ( i = 0; i < nbr_bin; i ++){
+        hist_out[i] = 0;
+    }
+
+    for ( i = 0; i < img_size; i ++){
+        hist_out[img_in[i]] ++;
+    }
+}
+
+void histogram_equalization(unsigned char * img_out, unsigned char * img_in, 
+                            int * hist_in, int img_size, int nbr_bin){
+    int *lut = (int *)malloc(sizeof(int)*nbr_bin);
+    int i, cdf, min, d;
+    /* Construct the LUT by calculating the CDF */
+    cdf = 0;
+    min = 0;
+    i = 0;
+    while(min == 0){
+        min = hist_in[i++];
+    }
+    d = img_size - min;
+    for(i = 0; i < nbr_bin; i ++){
+        cdf += hist_in[i];
+        //lut[i] = (cdf - min)*(nbr_bin - 1)/d;
+        lut[i] = (int)(((float)cdf - min)*255/d + 0.5);
+        if(lut[i] < 0){
+            lut[i] = 0;
+        }
+        
+        
+    }
+    
+    /* Get the result image */
+    for(i = 0; i < img_size; i ++){
+        if(lut[img_in[i]] > 255){
+            img_out[i] = 255;
+        }
+        else{
+            img_out[i] = (unsigned char)lut[img_in[i]];
+        }
+        
+    }
+}
+
+__global__ void gpu_histogram(int * hist_out, unsigned char * img_in, int * img_size, int * nbr_bin){
+	int id = threadIdx.x;
+    int i;
+    //for ( i = 0; i < nbr_bin; i ++){
+	if (id < *nbr_bin) {
+        hist_out[id] = 0;
+	}
+    //}
+
+    //for ( i = 0; i < img_size; i ++){
+	if (id < *img_size) {
+        hist_out[img_in[id]] ++;
+	}
+    //}
+}
+
+void gpu_histogram_equalization(unsigned char * img_out, unsigned char * img_in, 
+                            int * hist_in, int img_size, int nbr_bin){
+    int *lut = (int *)malloc(sizeof(int)* nbr_bin);
+    int i, cdf, min, d;
+    /* Construct the LUT by calculating the CDF */
+    cdf = 0;
+    min = 0;
+    i = 0;
+    while(min == 0){
+        min = hist_in[i++];
+    }
+    d = img_size - min;
+    for(i = 0; i < nbr_bin; i ++){
+        cdf += hist_in[i];
+        //lut[i] = (cdf - min)*(nbr_bin - 1)/d;
+        lut[i] = (int)(((float)cdf - min)*255/d + 0.5);
+        if(lut[i] < 0){
+            lut[i] = 0;
+        }
+        
+        
+    }
+    
+    /* Get the result image */
+    for(i = 0; i < img_size; i ++){
+        if(lut[img_in[i]] > 255){
+            img_out[i] = 255;
+        }
+        else{
+            img_out[i] = (unsigned char)lut[img_in[i]];
+        }
+        
+    }
+}
+
+void gpu_histogram_equalization_calc(unsigned char * img_out, unsigned char * img_in, 
+                            int * hist_in, int * lut, int * img_size, int * nbr_bin, int * min){
+    int i, cdf, min, d;
+    /* Construct the LUT by calculating the CDF */
+    cdf = 0;
+    min = 0;
+    i = 0;
+    while(min == 0){
+        min = hist_in[i++];
+    }
+    d = img_size - min;
+    for(i = 0; i < nbr_bin; i ++){
+        cdf += hist_in[i];
+        //lut[i] = (cdf - min)*(nbr_bin - 1)/d;
+        lut[i] = (int)(((float)cdf - min)*255/d + 0.5);
+        if(lut[i] < 0){
+            lut[i] = 0;
+        }
+        
+        
+    }
+    
+    /* Get the result image */
+    for(i = 0; i < img_size; i ++){
+        if(lut[img_in[i]] > 255){
+            img_out[i] = 255;
+        }
+        else{
+            img_out[i] = (unsigned char)lut[img_in[i]];
+        }
+        
+    }
+}
